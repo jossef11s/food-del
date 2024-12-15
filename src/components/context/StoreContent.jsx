d253b7bf97b70bd8819c1db98a6907a0c1c01ff7
jossef11s <youssefbettaib986@gmail.com>
2024-12-15 19:36:57 +0100

    deploy

--- src/components/context/StoreContent.jsx
+++ src/components/context/StoreContent.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000"
+  const url = "https://food-del-back-n8yn.onrender.com"
   const [token, setToken] = useState("");
   const [foodList, setFoodList] = useState([]);
 
